19871663685ce74e421ae65ef29fb5cac55b20f9
Daniel Zerwes Hunsche <daniel.hunsche@gmail.com>
2024-04-03 21:22:30 -0300

    site produtos

--- site.php
+++ site.php
@@ -1,11 +1,16 @@
 <?php
 
 use \hcode\Page;
+use \hcode\Model\Product;
 
 $app->get('/', function() {
-    
+
+	$products = Product::listAll();
+
 	$page = new Page();
-	$page->setTpl("index");
+	$page->setTpl("index", array(
+		"products"=>Product::checkList($products)
+	));
 
 });
 
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -14,6 +14,20 @@ class Product extends Model {
         return $sql->select("SELECT * FROM tb_products ORDER BY desproduct");
     }
 
+    public static function checkList($list)
+    {
+
+        foreach ($list as &$row) {
+
+            $p = new Product();
+            $p->setData($row);
+            $row = $p->getValues();
+
+        }
+
+        return $list;
+    }
+
     public function save() 
     {
 
@@ -70,7 +84,7 @@ class Product extends Model {
     public function checkPhoto()
     {
 
-        if(file_exists($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "site" . DIRECTORY_SEPARATOR . "products" . DIRECTORY_SEPARATOR . $this->getidproduct() . ".jpg")) {
+        if(file_exists($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "site" . DIRECTORY_SEPARATOR . "img" . DIRECTORY_SEPARATOR . "products" . DIRECTORY_SEPARATOR . $this->getidproduct() . ".jpg")) {
 
             $url = "res/site/img/products/" . $this->getidproduct() . ".jpg";
 
@@ -107,7 +121,7 @@ class Product extends Model {
 
         }
 
-        $dist = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "site" . DIRECTORY_SEPARATOR . "products" . DIRECTORY_SEPARATOR . $this->getidproduct() . ".jpg";
+        $dist = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "site" . DIRECTORY_SEPARATOR . "img" . DIRECTORY_SEPARATOR . "products" . DIRECTORY_SEPARATOR . $this->getidproduct() . ".jpg";
 
         imagejpeg($image, $dist);
 
